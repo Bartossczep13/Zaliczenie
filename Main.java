c1cc7b0dff0d31635923373e7bfafda2f3451ef6
TeO <100691829+Bartossczep13@users.noreply.github.com>
2022-06-21 11:50:28 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main {
+  
+  public static void main(String[] args) {
+    int[] numArray = {2, 6, 7, 6, 2, 19, 1, 19};
+    for(int i = 0; i < numArray.length; i++){
+      for(int j = i + 1; j < numArray.length; j++){
+        if(numArray[i] == numArray[j]){
+          System.out.println("Powtarzajacy sie element " + numArray[j]);
+        }
+      }
+    }    
+  }
+}
